25e91cf4e46bb3900b9d998bc56187825bdddbd1
Abdalhadi Darkoushe <abdalhadi.darkoushe@elev.ga.ntig.se>
2024-11-20 15:03:52 +0100

    iugyliug

---
+++ Steg1/Program.cs
@@ -0,0 +1,55 @@
+﻿
+ //if (6 >= 3) 
+ //{
+   //  Console.WriteLine("Hello World");
+ //}
+
+//Console.WriteLine("Abood");
+
+
+ 
+ 
+ 
+ //sting name = Console.ReadLine();
+
+//if(name == "kalleanka")
+//{
+  //Console.WriteLine("Welcome!"); 
+    // avänder namn "Abood" är = kalleanka   då det kommer "Welcome!
+//}
+
+
+
+
+
+//string password = "12345";
+//string name = "kaleanka";
+//Console.WriteLine("Enter your username: ");
+
+//string answername = Console.ReadLine();
+
+
+//Console.WriteLine("Enter your password: ");
+//string answerpassword = Console.ReadLine();
+
+//if(name == answername && password == answerpassword)
+//{
+  //Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//Console.WriteLine("Fel");
+//}
+
+
+
+
+
+
+
+string Hello = "Hello, word";
+for(int i = 0; i < 32; i++)
+{
+  Console.WriteLine(Hello);
+}
+Console.ReadLine();
